b87e7f1710e34f9f3260bd802ed3babc5973135a
ARROJUSRINIVAS702 <arrojubalu3@gmail.com>
2025-08-09 11:40:43 +0530

    succesfully uploaded arrays

---
+++ Arrays/PrintingElements.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class PrintingElements {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//program to print elements with static values
+
+		int a[]= {1,2,3,4,5};
+		System.out.println(Arrays.toString(a));
+	}
+
+}
